62ed26887d79f6c793b2f14aad3cf6541d78d7cd
Xuedixuedi <958190137@qq.com>
2020-07-18 23:46:56 +0800

    init

---
+++ src/components/Page1/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="head">
+        <div>hello world</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+.head {
+    width: 1920px;
+    height: 75px;
+    background-color: aliceblue;
+    color: aliceblue;
+}
+</style>
---
+++ src/components/Page1/Page1.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="bg">
+            <header></header>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from "./Header.vue"
+export default {
+    name: "Page1",
+    components: {
+        Header
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bg {
+    background-color: #202c36;
+    top: 0px;
+    width: 1920px;
+    height: 1024px;
+}
+</style>
