46d013037f4684528d44aa39a8c18b9b30a3da60
Xuedixuedi <958190137@qq.com>
2020-07-21 17:09:05 +0800

    not use mock but use 3 same card T.T

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from "vue"
 import App from "./App"
 import router from "./router"
 
-//视频播放
+//视频播放git
 import VideoPlayer from "vue-video-player"
 import "vue-video-player/src/custom-theme.css"
 import "video.js/dist/video-js.css"
